2eed10be1b827165c0c73baefe4816ba8f21677b
Saidgani Musaev <s.musaev@student.vu.nl>
2019-04-01 07:08:59 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -143,7 +143,7 @@ public class Main {
 
         // link to a parent
         if(!result[0].initiator)
-        result[0].links. add(new NodeInstance(new Pair<String, Integer>(das4_network.get(parentid).substring(8), 1111),das4_network.indexOf(das4_network.get(parentid)) + 1));
+            result[0].links. add(new NodeInstance(new Pair<String, Integer>(das4_network.get(parentid).substring(8), 1111),das4_network.indexOf(das4_network.get(parentid)) + 1));
 
         // links to children
         if(childid < das4_network.size())
